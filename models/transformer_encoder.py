fd665f24e4d24b32034183bf6260d54475a3faa2
Luphuc2005 <phucga150625@gmail.com>
2026-03-24 11:02:46 +0700

    24032026 update

--- models/transformer_encoder.py
+++ models/transformer_encoder.py
@@ -2,15 +2,24 @@ import torch
 import torch.nn as nn
 import torchvision.models as models
 class TransformerEncoder(nn.Module):
-    def __init__(self, embed_dim=512, num_heads=1):
+    def __init__(self, embed_dim=512, num_heads=8):
         super().__init__()
-        self.norm=nn.LayerNorm(embed_dim)
-        self.attn=nn.MultiheadAttention(embed_dim,num_heads,batch_first=True)
-        self.ff=nn.Linear(embed_dim,embed_dim)
-    def forward(self,x):
-        x=self.norm(x)
-        att_out,_=self.attn(x,x,x)
-        x=x+att_out
-        x=self.norm(x)
-        x=self.ff(x)
+        self.norm1 = nn.LayerNorm(embed_dim)
+        self.norm2 = nn.LayerNorm(embed_dim)
+
+        self.attn = nn.MultiheadAttention(embed_dim, num_heads, batch_first=True)
+
+        self.ff = nn.Sequential(
+            nn.Linear(embed_dim, 2048),
+            nn.ReLU(),
+            nn.Linear(2048, embed_dim)
+        )
+
+    def forward(self, x):
+        # Attention block
+        x = x + self.attn(self.norm1(x), self.norm1(x), self.norm1(x))[0]
+
+        # Feedforward block
+        x = x + self.ff(self.norm2(x))
+
         return x
